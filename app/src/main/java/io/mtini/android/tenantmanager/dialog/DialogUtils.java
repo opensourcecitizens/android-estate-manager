3abf45c0fb260cb495438426dbf57905a74610dd
kaniundungu <kaniu@mtini.io>
2018-11-21 17:49:02 -0800

    alert dialog utility

--- app/src/main/java/io/mtini/android/tenantmanager/dialog/DialogUtils.java
+++ app/src/main/java/io/mtini/android/tenantmanager/dialog/DialogUtils.java
@@ -1,7 +1,12 @@
 package io.mtini.android.tenantmanager.dialog;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.app.ProgressDialog;
 import android.content.Context;
+import android.content.DialogInterface;
+
+import io.mtini.android.tenantmanager.R;
 
 public class DialogUtils {
 
@@ -16,4 +21,23 @@ public class DialogUtils {
 
         return dialog;
     }
+
+
+    public static AlertDialog startErrorDialog(Context context, String message){
+
+        AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(context);// this = YourActivity
+        //TODO add icon dialogBuilder.setIcon()
+        dialogBuilder.setMessage(message)
+                .setTitle("Error");
+        dialogBuilder.setPositiveButton("Ok",new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                //dismiss();
+            }
+        });
+
+        AlertDialog errorDialog = dialogBuilder.create();
+        errorDialog.show();
+        return errorDialog;
+    }
+
 }
